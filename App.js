128ced36a4d8ce51f347e444bab0f2875881cbb6
shirleytique <shirleytique911@gmail.com>
2024-04-23 02:09:03 -0500

    iniciando proyecto con expo

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Pressable onPress={{} - alert('hello shirley') //se implementa esta prueba para comprobar funcionalidad en web
+      }>
+        <text>press me</text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
